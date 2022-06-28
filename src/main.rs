6ed4f101a1856da57a3c988841e09edf9feb2c31
Cameron Daly <camerondaly9@gmail.com>
2022-06-28 16:45:39 -0600

    fixed typo and printed simple tests.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@ use std::io;
 fn main() {
     let n: u32 = get_positive_number_from_user();
     let nth = find_nth_fibonacci_number(n);
-    println!("The {}th Fibonacci numnber is {}!", n, nth);
+    println!("The {}th Fibonacci number is {}!", n, nth);
+    test();
 }
 
 
 fn find_nth_fibonacci_number(n: u32) -> u128 {
-    // 0,1,2,3,5,8,13,...
+    // 1,2,3,5,8,13,...
     // O(n) time and constant space complexity with dynamic memoization.
-    if n <= 3 {
+    if n <= 2 {
         return n.into();
     }
     let mut two_prev = 1;
     let mut one_prev = 2;
     let mut curr: u128 = 3;
-    for _ in 4..n + 1 {
+    for _ in 3..n + 1 {
         curr = two_prev + one_prev;
         two_prev = one_prev;
         one_prev = curr;
@@ -35,5 +36,20 @@ fn get_positive_number_from_user() -> u32 {
             Err(_) => continue,
         };
         return n;
+    }
+}
+
+fn test() {
+    let pass: bool = 
+        1 == find_nth_fibonacci_number(1) && 
+        2 == find_nth_fibonacci_number(2) && 
+        3 ==find_nth_fibonacci_number(3) && 
+        5 == find_nth_fibonacci_number(4) && 
+        8 == find_nth_fibonacci_number(5) && 
+        13 == find_nth_fibonacci_number(6);
+    if pass == true {
+        println!("Tests passed.");
+    } else {
+        println!("Tests failed!");
     }
 }
